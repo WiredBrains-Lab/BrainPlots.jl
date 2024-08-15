1278655c4dadd4e795191d8a85687cea6356d3ea
Bill Gross <azraq27@users.noreply.github.com>
2024-08-14 19:58:13 -0500

    fsaverage path

--- src/BrainPlots.jl
+++ src/BrainPlots.jl
@@ -1,5 +1,16 @@
 module BrainPlots
 
-greet() = print("Hello World!")
+import Pkg.Artifacts
+
+function fsaverage_path(;hemi=:left)
+	if hemi==:left
+		return joinpath(artifact"fsaverage","fsaverage_pial_lh.stl")
+	elseif hemi==:right
+		return joinpath(artifact"fsaverage","fsaverage_pial_rh.stl")
+	else
+		error("Invalid arguments to `hemi`: only `:left` or `:right` allowed")
+	end
+end
+
 
 end # module BrainPlots
